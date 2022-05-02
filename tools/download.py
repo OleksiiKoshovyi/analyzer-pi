b7bc2d094d8e18f1dc525ff9a4d729e6eeea302a
nebosky142 <45599477+OleksiiKoshovyi@users.noreply.github.com>
2022-05-02 15:46:08 +0200

    create aqusition script for daqhats
    create download/upload tools

---
+++ tools/download.py
@@ -0,0 +1,17 @@
+import json
+import subprocess
+import click
+
+
+print('You are going to download scripts from raspberry-pi.')
+if not click.confirm('It may replace old files. Continue?', default=True):
+    exit
+
+with open('../config.json', encoding='utf-8', mode='r') as config_file:
+    config = json.load(config_file)
+    user = config['user']
+    host = config['host']
+    connection = '{}@{}:'.format(user, host)
+    subprocess.run( ["scp", "-r", "-P", "22",
+        connection + "daqhats/examples/python/mcc128/\{values_reader*,/samples\}",
+        "../scripts/"] )
