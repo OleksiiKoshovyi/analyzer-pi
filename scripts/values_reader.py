4d3e3c1469bbbd6947dd276e267b154c172fa582
nebosky142 <45599477+OleksiiKoshovyi@users.noreply.github.com>
2022-05-18 10:25:30 +0200

    refactor code; create readme

--- scripts/values_reader.py
+++ scripts/values_reader.py
@@ -17,6 +17,8 @@
 from __future__ import print_function
 from time import sleep
 from sys import stdout
+
+from pandas import Interval
 from daqhats import mcc128, OptionFlags, HatIDs, HatError, AnalogInputMode, \
     AnalogInputRange
 from daqhats_utils import select_hat_device, enum_mask_to_string, \
@@ -24,35 +26,75 @@ from daqhats_utils import select_hat_device, enum_mask_to_string, \
 from datetime import datetime
 import csv
 import json
+from types import SimpleNamespace
+
 
 # Constants
 CURSOR_BACK_2 = '\x1b[2D'
 ERASE_TO_END_OF_LINE = '\x1b[0K'
 
 
-def configure_script():
+def get_config():
     with open('./config.sampling.json', encoding='utf-8', mode='r') as config_file:
-        config = json.load(config_file)
-        channels = config['channels']
-        duration = config['duration']
-        interval = config['interval']
-        output_config = config['output_config']
-        return channels, duration, interval, output_config
+        config = json.load(config_file, object_hook=lambda d: SimpleNamespace(**d))
+        return config
+
+def sample_voltage(hat, options, config):
+    duration = config.duration
+    sample_interval = config.interval
+    channels = config.channels
+    if len(channels) == 0:
+        print('You didn\'t select any channel in config')
+        return []
+    start_time = datetime.now().timestamp()
+    end_time = start_time + duration
+    samples = []
+    while datetime.now().timestamp() < end_time:
+        # Read a single value from each selected channel.
+        for chan in channels:
+            value = hat.a_in_read(chan, options)
+            time = str(datetime.now().timestamp())
+            samples.append({'date': time, 'channel': chan, 'sample': value})
+        # Wait the specified interval between reads.
+        sleep(sample_interval)
+    samples_count = len(samples)
+    channels_count = len(channels)
+    samples_per_channel = int(samples_count / channels_count)
+    print('samples per channel: ', samples_per_channel)
+    return samples
+
+def save_result_to_csv(samples, start_time):
+    file_name = 'samples/voltage-{}.csv'.format(str(start_time))
+    fieldnames = ['date', 'channel', 'sample']
+    with open(file_name, 'w', newline='') as csv_file:
+        print('new file created: {}'.format(file_name))
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(samples)
+
+def print_result_to_console(samples):
+    for sample in samples:
+        print('{:>}. Ch: {}. {:12.7} V'.format(sample['date'], sample['channel'], sample['sample']))
+
+def print_result(config, samples, start_time):
+    output_sources = config.output_sources
+    if output_sources.console:
+        print_result_to_console(samples)
+    if output_sources.csv:
+        save_result_to_csv(samples, start_time)
 
 def main():
     """
     This function is executed automatically when the module is run directly.
     """
-    channels, duration, interval, output_config = configure_script()
+    config = get_config()
     options = OptionFlags.DEFAULT
     low_chan = 0
     high_chan = 3
-    channel_count = len(channels)
     input_mode = AnalogInputMode.SE
     input_range = AnalogInputRange.BIP_10V
 
     mcc_128_num_channels = mcc128.info().NUM_AI_CHANNELS[input_mode]
-    sample_interval = interval  # Seconds
 
     try:
         # Ensure low_chan and high_chan are valid.
@@ -84,7 +126,9 @@ def main():
         print('    Input mode: ', input_mode_to_string(input_mode))
         print('    Input range: ', input_range_to_string(input_range))
         print('    Channels: {0:d} - {1:d}'.format(low_chan, high_chan))
-        print('    Selected channels: {}'.format(channels))
+        print('    Selected channels: {}'.format(config.channels))
+        print('    Sampling interval: {}'.format(config.interval))
+        print('    Sampling duration: {}'.format(config.duration))
         print('    Options:', enum_mask_to_string(OptionFlags, options))
         try:
             input("\nPress 'Enter' to continue")
@@ -93,43 +137,11 @@ def main():
 
         print('\nAcquiring data ... Press Ctrl-C to abort')
 
-        # Display the header row for the data table.
-        print('\nSample')
-
         try:
-            samples_per_channel = 0
-            time = str(datetime.now().timestamp())
-            file_name = 'samples/voltage-{}.csv'.format(time)
-            fieldnames = ['date', 'channel', 'sample']
-            with open(file_name, 'w', newline='') as csv_file:
-                print('new file created: {}'.format(file_name))
-                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                writer.writeheader()
-                samples = [{}] * channel_count
-                start_time = datetime.now().timestamp()
-                end_time = start_time + duration
-                while datetime.now().timestamp() < end_time:
-                    # Display the updated samples per channel count
-                    samples_per_channel += 1
-                    print('{:17}'.format(samples_per_channel))
-
-                    # Read a single value from each selected channel.
-                    for id, chan in enumerate(channels):
-                        value = hat.a_in_read(chan, options)
-                        time = str(datetime.now().timestamp())
-                        samples[id] = {'date': time, 'channel': chan, 'sample': value}
-
-                    stdout.flush()
-                    # Save in csv
-                    if output_config['save_result_in_csv']:
-                        writer.writerows(samples)
-                    # Print in console
-                    if output_config['print_result']:
-                        for sample in samples:
-                            print('{:>}. Ch: {}. {:12.7} V'.format(sample['date'], sample['channel'], sample['sample']))
-
-                    # Wait the specified interval between reads.
-                    sleep(sample_interval)
+            start_time = datetime.now().timestamp()
+            samples = sample_voltage(hat, options, config)
+            print_result(config, samples, start_time)
+            print('Done')
 
         except KeyboardInterrupt:
             # Clear the '^C' from the display.
